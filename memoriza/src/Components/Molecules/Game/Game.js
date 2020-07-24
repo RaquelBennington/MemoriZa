e35b9d979075eea725f7421380d56874d646dd54
Raquel Bennington <raquelbennington@gmail.com>
2020-07-24 10:27:45 -0300

    atualização final

--- memoriza/src/Components/Molecules/Game/Game.js
+++ memoriza/src/Components/Molecules/Game/Game.js
@@ -1,23 +1,37 @@
 import React from "react";
-import FlipFront1 from "../../../Assets/1.png"
-import FlipBack2 from "../../../Assets/2.png"
-import FlipFront3 from "../../../Assets/3.png"
-import FlipBack4 from "../../../Assets/4.png"
 import "./game.css"
+
+const letter = [
+    { front: "1.png", back: "2.png" },
+    { front: "3.png", back: "4.png" },
+    { front: "5.png", back: "6.png" },
+    { front: "7.png", back: "8.png" },
+    { front: "9.png", back: "10.png" },
+    { front: "11.png", back: "12.png" },
+    { front: "13.png", back: "14.png" },
+    { front: "15.png", back: "16.png" },
+
+]
 
 const Game = () => {
     return (
         <section className="container">
+
             <div className="flip-card-container">
-                <div className="flip-card">
-                    <div className="flip-card-front">
-                        <img src={FlipFront1} alt="Card Front" />
-                    </div>
-                    <div className="flip-card-back">
-                        <img src={FlipBack2} alt="Card Back" />
-                    </div>
-                </div>
+            {letter.map((item) => {
+                return (
+                        <div className="flip-card">
+                            <div className="flip-card-front">
+                                <img src={`${process.env.PUBLIC_URL}/img/${item.front}`} alt="Card Front" />
+                            </div>
+                            <div className="flip-card-back">
+                            <img src={`${process.env.PUBLIC_URL}/img/${item.back}`} alt="Card Back" />
+                            </div>
+                        </div>
+                )
+            })}
             </div>
+
         </section>
     )
 }
