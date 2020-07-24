8ec77d67b061cc41d1282cf2b6b80f7d53e39943
Raquel Bennington <raquelbennington@gmail.com>
2020-07-24 02:07:24 -0300

    alinhamento

--- memoriza/src/Components/Molecules/SectionGame/SectionGame.js
+++ memoriza/src/Components/Molecules/SectionGame/SectionGame.js
@@ -4,18 +4,13 @@ import "../SectionGame/sectionGame.css"
 
 const SectionGame = () => {
     return (
-        <>
-            <section>
-                <div>
-                    <h1 className="First_Title">Bora lá, agora é hora de memorizar</h1>
-                </div>
-            </section>
             <section class="SectionGame">
                 <div className="vazia" >
                     <div className="First_Section">
-                        <p>vamos memorizar?</p>
+                        <h1>Você conhece os Operadores de Comparação?</h1>
                         <div className="btnTwo">
-                            <button className="btn_startGame1">Saiba mais</button>
+                            <button className="btn_startGame1">Saiba + </button>
+                            <button className="white"></button>
                             <button className="btn_startGame2">Jogar</button>
                         </div>
                     </div>
@@ -24,7 +19,6 @@ const SectionGame = () => {
                     <img className="AssetsGame" src={AssetsGame} alt="Imagem jogo da Memória" />
                 </div>
             </section>
-        </>
     )
 };
 
